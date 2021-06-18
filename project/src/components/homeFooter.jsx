1a9e1a66bf2bb4fb290d06c99ee3d87dc734208d
yufu <964345264@qq.com>
2021-06-18 11:11:21 +0800

    change footer

--- project/src/components/homeFooter.jsx
+++ project/src/components/homeFooter.jsx
@@ -5,7 +5,7 @@ export default class Homefooter extends Component {
         return (
             <div id='home-footer'>
                 <div className="center">
-                    <p>Copyright © 2021 渔夫yufu 备案号：xxxxx（备案中）</p>
+                    <p>Copyright © 2021 渔夫yufu 备案号：<a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank" rel="noopener noreferrer">桂ICP备2021004940号</a></p>
                     <p>本站由本人自主设计与搭建</p>
                     <p>**图片素材来自网络，如有侵权可向我告知进行删除**</p>
                 </div>
